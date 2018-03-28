383fa9b8888f4712fb3d45e1201bbfb4a1cffeae
Arnas <arnas@ms-7994.lan>
2018-03-28 18:06:25 +0300

    Update Negative Values

--- assets/js/javascript.js
+++ assets/js/javascript.js
@@ -1,10 +1,20 @@
 function numberConversion() {
   var myNumber = parseInt(document.getElementById("number").value);
   if (numberValidation(myNumber)) {
-    document.getElementById("result").innerHTML = myNumber + " = " + myNumber + " Decimal, " +
-    myNumber.toString(16) + " Hexadecimal, " +
-    myNumber.toString(8) + " Octal, " +
-    myNumber.toString(2) + " Binary."
+    var hexadecimal = myNumber.toString(16);
+    var octal = myNumber.toString(8);
+    var binary = myNumber.toString(2);
+    if (myNumber < 0) {
+      myNumber = -myNumber;
+      hexadecimal = hexadecimal.slice( 1 );
+      octal = -octal;
+      binary = -binary;
+    }
+    document.getElementById("result").innerHTML = myNumber + " = " +
+    myNumber + " Decimal, " +
+    hexadecimal + " Hexadecimal, " +
+    octal + " Octal, " +
+    binary + " Binary."
   } else {
     document.getElementById("result").innerHTML = "Your query wasn't a number";
   }
